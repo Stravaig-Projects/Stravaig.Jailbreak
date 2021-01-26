0c8827a72aea3c12cfd150c685cacc3f59768fc8
Colin Mackay <colinangusmackay@users.noreply.github.com>
2021-01-26 21:55:05 +0000

    Initial commit

---
+++ Reset-WipReleaseNotes.ps1
@@ -0,0 +1,20 @@
+$content = @(
+"# Release Notes",
+"",
+"## Version X",
+"",
+"Date: ???",
+""
+"### Bugs"
+"",
+"### Features",
+"",
+"### Miscellaneous",
+"",
+"### Dependabot",
+"",
+"",
+""
+)
+
+Set-Content "$PSScriptRoot/release-notes/wip-release-notes.md" $content -Encoding UTF8 -Force
